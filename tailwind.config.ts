5d93133c6f9de7d5bff461a33d6f81b59edfd585
Anas Abdallah Nady <eng.anas.nady@gmail.com>
2024-08-11 10:44:37 +0300

    Remove src/pages/**/* from Tailwind CSS content paths.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,6 @@ import flowbite from "flowbite-react/tailwind";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     flowbite.content(),
